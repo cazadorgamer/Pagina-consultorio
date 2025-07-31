7643bb7fb73988087623b4ee9e424221be1d5398
Facundo Martinez <lasocholetras@gmail.com>
2025-07-31 16:15:55 -0300

    update
    comienzo de implementacion de javascript

---
+++ HistClin.js
@@ -0,0 +1,8 @@
+alert("hola chiche")
+
+var Numero=document.getElementById(exampleFormControlInput3);
+var nombrePaciente=document.getElementById(exampleFormControlInput1);
+var dni=document.getElementById(exampleFormControlInput2);
+var especialista=document.getElementById(especialista);
+var Direccion=document.getElementById(exampleFormControlInput4);
+var Direccion=document.getElementById(exampleFormControlTextarea1;
---
+++ paciente.js
@@ -0,0 +1,6 @@
+alert("hola chiche")
+
+var dni=document.getElementById(dni);
+var nombre=document.getElementById(nombre);
+var fecha=document.getElementById(fecha);
+var especialista=document.getElementById(especialista);
